c67c142e48d873ccf3d425fe67a01bb4b00eea28
Nette Fornaleza <monette.fornaleza@smsupermalls.com>
2025-08-21 21:01:40 +0800

    Revert to First Commit

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 def main():
-    x = input("Enter your name: ")
-    if x == "":
-        print("you didnt type anything")
-        y = input()
-        if y == "":
-            print("ok whatever")
-        else:
-            print("hello " + y)
-    else:
-        print("hello " + x)
+    print("Hello, world!")
 
-main()
+
+if __name__ == "__main__":
+    main()
